2d7f008d1db51dba396f82ec9a22b69571981f25
lecon20122 <lecon20122@gmail.com>
2022-04-16 15:10:30 +0200

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 import HomePage from './pages/homepage/homepage.page';
 
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
