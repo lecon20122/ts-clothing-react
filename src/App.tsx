eed36b9e81e7c729438627595fb0654275effc2e
lecon20122 <lecon20122@gmail.com>
2022-05-04 15:01:14 +0200

    saving old changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/header/header-component';
 import HomePage from './pages/homepage/homepage.page';
-import ShopPage from "./pages/shop/shop.page";
-
-
+import ShopPage from './pages/shop/shop.page';
+import './App.scss';
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/shop" element={<ShopPage />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/shop" element={<ShopPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
